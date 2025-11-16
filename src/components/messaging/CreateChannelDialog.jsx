570aae60658751b696fa2384296b5c5afa01744a
Martin Lopera <mloperacde@gmail.com>
2025-11-16 18:59:44 +0000

    File changes

--- src/components/messaging/CreateChannelDialog.jsx
+++ src/components/messaging/CreateChannelDialog.jsx
@@ -1,6 +1,7 @@
+
 import React, { useState, useMemo } from "react";
 import { base44 } from "@/api/base44Client";
-import { useMutation, useQuery, useQueryClient } from "@tantml:react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
