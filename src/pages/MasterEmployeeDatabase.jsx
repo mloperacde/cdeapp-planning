33ba4bdd076ad4b1b955508e5a85555ed9012992
Martin Lopera <mloperacde@gmail.com>
2025-11-22 15:23:16 +0000

    File changes

--- src/pages/MasterEmployeeDatabase.jsx
+++ src/pages/MasterEmployeeDatabase.jsx
@@ -28,6 +28,7 @@ import { Link } from "react-router-dom";
 import { createPageUrl } from "@/utils";
 import { format } from "date-fns";
 import { es } from "date-fns/locale";
+import { toast } from "sonner";
 import MasterEmployeeImport from "../components/master/MasterEmployeeImport";
 import SyncComparisonDialog from "../components/master/SyncComparisonDialog";
 import SyncHistoryPanel from "../components/master/SyncHistoryPanel";
