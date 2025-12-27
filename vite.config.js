1e00ad26d9821dc053c720757184b267d391599c
mloperacde <mloperacde@gmail.com>
2025-12-27 19:29:32 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-// NO importes '@tailwindcss/vite' si no lo instalaste
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  server: {
+    allowedHosts: [
+      'ta-01kdge0mgeee2ynvp3r34kqmyf-5173.wo-gst0pxk9x5561n5uewg43v7i5.w.modal.host'
+    ]
+  }
+})
