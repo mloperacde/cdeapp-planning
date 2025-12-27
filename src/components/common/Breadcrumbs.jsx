881e815bc38c153d95733e03266ff477a4341154
mloperacde <mloperacde@gmail.com>
2025-12-27 22:39:07 +0100

    Update Breadcrumbs.jsx

--- src/components/common/Breadcrumbs.jsx
+++ src/components/common/Breadcrumbs.jsx
@@ -1,1 +1,16 @@
+// src/components/common/Breadcrumbs.jsx
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
+export default function Breadcrumbs() {
+  const location = useLocation();
+  
+  return (
+    <div className="text-sm breadcrumbs">
+      <ul>
+        <li><Link to="/">Inicio</Link></li>
+        {/* Añadir lógica según tu necesidad */}
+      </ul>
+    </div>
+  );
+}
