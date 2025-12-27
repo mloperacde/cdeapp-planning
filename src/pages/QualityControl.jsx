4c6ee4a82e1a99bd165a2ae07176688ba900fa1e
mloperacde <mloperacde@gmail.com>
2025-12-27 22:35:46 +0100

    Update QualityControl.jsx

--- src/pages/QualityControl.jsx
+++ src/pages/QualityControl.jsx
@@ -14,8 +14,8 @@ import { format, parseISO } from "date-fns";
 import { es } from "date-fns/locale";
 import { toast } from "sonner";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import EmployeeSelect from "../components/common/EmployeeSelect";
-import Breadcrumbs from "../components/common/Breadcrumbs";
+import EmployeeSelect from "@/components/common/EmployeeSelect";
+import Breadcrumbs from "@/components/common/Breadcrumbs";
 import { createPageUrl } from "@/utils";
 
 export default function QualityControlPage() {
