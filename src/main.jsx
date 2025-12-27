0cbc0f6b847dd713e87873a10556a7a63a0b0c60
mloperacde <mloperacde@gmail.com>
2025-12-27 19:21:05 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -89,14 +89,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>
 );
 
-if (import.meta.hot) {
-  import.meta.hot.on('vite:beforeUpdate', () => {
-    window.parent?.postMessage({ type: 'sandbox:beforeUpdate' }, '*');
-  });
-  import.meta.hot.on('vite:afterUpdate', () => {
-    window.parent?.postMessage({ type: 'sandbox:afterUpdate' }, '*');
-  });
-}
-
-
 
