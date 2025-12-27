0e8b8ce790f91de37b161132db277925b58fb175
mloperacde <mloperacde@gmail.com>
2025-12-27 01:22:20 +0100

    Add TestServices to pages configuration

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,3 +1,4 @@
+import TestServices from './pages/TestServices';
 import AbsenceManagement from './pages/AbsenceManagement';
 import AbsenceTypeConfig from './pages/AbsenceTypeConfig';
 import AbsenceTypeInfo from './pages/AbsenceTypeInfo';
@@ -172,6 +173,7 @@ export const PAGES = {
     "UserInvitations": UserInvitations,
     "UserManual": UserManual,
     "WorkCalendarConfig": WorkCalendarConfig,
+    "TestServices": TestServices,
 }
 
 export const pagesConfig = {
