04357fa257dd89da64a4074849dd11779944f20b
mloperacde <mloperacde@gmail.com>
2025-12-27 19:24:14 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
-export default {
+module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    tailwindcss: {}, // Procesa Tailwind
+    autoprefixer: {}, // Añade prefijos para compatibilidad
   },
-}
+};
