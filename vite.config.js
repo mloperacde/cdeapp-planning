c67599ff23ff8bbb4448b1352eb8bff4b0f6876a
mloperacde <mloperacde@gmail.com>
2025-12-28 01:05:48 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'ta-01kdh44kwvcq98v3k52dg75k5a-5173.wo-e4jyv0c7tb4kme8lf4gyo2gyo.w.modal.host',
+      '.modal.host'  // Permite todos los subdominios de modal.host
+    ],
+    host: true,  // Importante para Base44
+    port: 5173
+  },
   build: {
     outDir: 'dist',
-    sourcemap: false
+    sourcemap: false,
+    emptyOutDir: true
   }
 })
