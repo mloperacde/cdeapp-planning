274fc94602f76dbb4555b65287f9b25727b5449f
mloperacde <mloperacde@gmail.com>
2025-12-28 03:38:48 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: true
+    allowedHosts: true,
+    host: true,
+    port: 5173
   },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-    extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx', '.json']
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
-}) 
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  }
+})
