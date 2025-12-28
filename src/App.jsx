d6b6501d033be54feea65505d39ee15bb648f954
mloperacde <mloperacde@gmail.com>
2025-12-28 04:38:57 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './Layout';
 import Dashboard from './pages/Dashboard';
+import AdvancedHRDashboard from './pages/AdvancedHRDashboard';
+import MasterEmployeeDatabase from './pages/MasterEmployeeDatabase';
+import MachineManagement from './pages/MachineManagement';
+import Timeline from './pages/Timeline';
 
 function App() {
   return (
@@ -9,6 +13,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard />} />
+          <Route path="hr-dashboard" element={<AdvancedHRDashboard />} />
+          <Route path="employees" element={<MasterEmployeeDatabase />} />
+          <Route path="machines" element={<MachineManagement />} />
+          <Route path="timeline" element={<Timeline />} />
         </Route>
       </Routes>
     </Router>
