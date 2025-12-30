25036d8ba4c869fb820e7c8cae1291e43a6e1355
mloperacde <mloperacde@gmail.com>
2025-12-30 18:12:27 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 import { defineConfig } from 'vite'
