40b4515c472085826a335e4acb292848971fbe78
mloperacde <mloperacde@gmail.com>
2026-01-08 21:19:16 +0100

    fix(vite): allow specific host in server.allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  server: {
+    allowedHosts: [
+      'ta-01kefkyhbqvw1720cj1wvg74z8-5173.wo-hjnvra6nj7a427e42226w9k84.w.modal.host'
+    ]
+  }
 })
