60f4b2601e3ddf5bf37f03388034002c2fdc12e2
mloperacde <mloperacde@gmail.com>
2026-01-26 22:37:20 +0100

    eliminar diagnostico de datos de ApiStatus

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import './App.css'
 import Pages from "@/pages/index.jsx"
 import { Toaster } from "@/components/ui/toaster"
-import ApiStatus from "@/components/debug/ApiStatus"
 
 function App() {
   return (
     <div className="notranslate" translate="no">
       <Pages />
       <Toaster />
-      <ApiStatus />
     </div>
   )
 }
