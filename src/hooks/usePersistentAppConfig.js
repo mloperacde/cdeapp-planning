2dc80cd44c4f225893d7f0a556f1c3c36f828700
mloperacde <mloperacde@gmail.com>
2026-02-07 22:17:02 +0100

    Fix_syntax_error_in_usePersistentAppConfig

--- src/hooks/usePersistentAppConfig.js
+++ src/hooks/usePersistentAppConfig.js
@@ -1,5 +1,5 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import base44 from '@/api/base44Client';
+import { base44 } from '@/api/base44Client';
 import { toast } from 'sonner';
 
 /**
