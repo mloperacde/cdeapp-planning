21b23f0b737d4ded799aba8fe3d997f83e3f59e1
mloperacde <mloperacde@gmail.com>
2026-02-12 21:37:44 +0100

    correccion errores

--- src/components/process-configurator/services/localDataService.js
+++ src/components/process-configurator/services/localDataService.js
@@ -314,7 +314,7 @@ export const localDataService = {
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
       
-      reader.onload = (e) => {
+      reader.onload = async (e) => {
         try {
           const data = new Uint8Array(e.target.result);
           const workbook = XLSX.read(data, { type: 'array' });
